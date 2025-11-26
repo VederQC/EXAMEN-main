d4e1d6a74d87bd5da532af5f6818990338ef2cdf
Veder202320611 <vedermw@gmail.com>
2025-11-26 01:00:09 -0500

    nuevo

--- n-t-pago/src/main/java/com/example/n_t_pago/repository/PagoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.n_t_pago.repository;
-
-
-
-import com.example.n_t_pago.entity.Pago;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PagoRepository extends JpaRepository<Pago, Long> {
-}
