f370785f8282ab1059e5fa2ed37689517af78f0a
Veder202320611 <vedermw@gmail.com>
2025-11-26 02:43:18 -0500

    maso

--- ms-contabilidad/src/main/java/com/example/ms_contabilidad/MsContabilidadApplication.java
+++ ms-contabilidad/src/main/java/com/example/ms_contabilidad/MsContabilidadApplication.java
@@ -2,8 +2,10 @@ package com.example.ms_contabilidad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.example.ms_contabilidad.feign")
 public class MsContabilidadApplication {
 
 	public static void main(String[] args) {
